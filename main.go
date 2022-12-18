b4a1318d61c6fd118421de794e3c704f490d1e19
dirkarnez <smalldirkalex@gmail.com>
2022-12-18 19:21:16 +0800

    - add ffmpeg, wip

--- main.go
+++ main.go
@@ -116,5 +116,97 @@ func main() {
 		})
 	})
 
+	app.Get("/ffmpeg", func(ctx iris.Context) {
+		ctx.ContentType("video/mp4")
+		ctx.Header("Transfer-Encoding", "chunked")
+
+		cmd := exec.Command("P:\\Downloads\\ffmpeg-2021-10-28-git-e84c83ef98-full_build\\bin\\ffmpeg.exe",
+			"-i",
+			`https://ewcdn04.nowe.com/session/09-ea4a143a0908c150afdfa113f8005/nowstreamer/getnowmediahls/NNEW00372892/sd/index.m3u8?token=2772bdf2ffc2cd8444ecade15a7e996b_1671374384`,
+			"-movflags", "frag_keyframe", "-f", "mp4", "pipe:1")
+
+		var stdout, stderr []byte
+		var errStdout, errStderr error
+		stdoutPipe, _ := cmd.StdoutPipe()
+		stderrIn, _ := cmd.StderrPipe()
+		err := cmd.Start()
+		if err != nil {
+			log.Fatalf("cmd.Start() failed with '%s'\n", err)
+		}
+
+		// cmd.Wait() should be called only after we finish reading
+		// from stdoutIn and stderrIn.
+		// wg ensures that we finish
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			//stdout, errStdout = copyAndCapture(os.Stdout, stdoutPipe)
+			defer wg.Done()
+
+			ctx.StreamWriter(func(w io.Writer) bool {
+				var out []byte
+				buf := make([]byte, 1024, 1024)
+				for {
+					n, err := stdoutPipe.Read(buf[:])
+					if n > 0 {
+						d := buf[:n]
+						out = append(out, d...)
+						_, err := w.Write(d)
+						if err != nil {
+							return false
+						} else {
+							return true
+						}
+					} else {
+						if err != nil {
+							// Read returns io.EOF at the end of file, which is not an error for us
+							if err == io.EOF {
+								return false // close and flush
+							}
+							return false
+							//return out, err
+						} else {
+							return true
+						}
+					}
+
+				}
+
+				// _, err := io.Copy(w, )
+				// if err != nil {
+				// 	if err == io.EOF {
+				// 		fmt.Println("EOF!!!!!")
+				// 		return false // close and flush
+				// 	}
+
+				// 	fmt.Println("err != nil!!!!!")
+				// 	return true
+				// } else {
+				// 	return true // continue write
+				// }
+			})
+		}()
+
+		//stderr, errStderr =
+		copyAndCapture(os.Stderr, stderrIn)
+
+		wg.Wait()
+
+		err = cmd.Wait()
+		if err != nil {
+			log.Fatalf("cmd.Run() failed with %s\n", err)
+		}
+		if errStdout != nil || errStderr != nil {
+			log.Fatal("failed to capture stdout or stderr\n")
+		}
+
+		// outStr, errStr := string(stdout), string(stderr)
+		// fmt.Printf("\nout:\n%s\nerr:\n%s\n", outStr, errStr)
+		errStr := string(stderr)
+		fmt.Printf("\nlen(out):\n%d\nerr:\n%s\n", len(stdout), errStr)
+		//reader := bytes.NewReader(stdout)
+
+	})
+
 	app.Run(iris.Addr(":8080"))
 }
